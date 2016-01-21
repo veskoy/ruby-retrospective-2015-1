9fd193ccc44098bd31ff0f43e4911868841098cf
V. Stoyanov <devphp.web@gmail.com>
2016-01-21 18:23:04 +0200

    Refactored solution for the 1st task.

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,7 +1,6 @@
-CURRENCIES = {bgn: 1, usd: 1.7408, eur: 1.9557, gbp: 2.6415}
-
 def convert_to_bgn(price, currency)
-  (price * CURRENCIES[currency]).round(2)
+  currencies = {bgn: 1, usd: 1.7408, eur: 1.9557, gbp: 2.6415}
+  (price * currencies[currency]).round(2)
 end
 
 def compare_prices(first_price, first_currency, second_price, second_currency)
