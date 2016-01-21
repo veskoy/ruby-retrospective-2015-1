60c2bfbae1bea4d63f3ddf3db0e6e57fbd2dbd10
V. Stoyanov <devphp.web@gmail.com>
2016-01-21 19:19:14 +0200

    Refactored second task code.

--- solutions/02.rb
+++ solutions/02.rb
@@ -1,38 +1,32 @@
 def move(snake, direction)
-  snake.shift
-  last_position = snake.last
-  new_position = [last_position[0]+direction[0], last_position[1]+direction[1]]
-  snake.push(new_position)
+  (new_snake = grow(snake, direction)).shift
+  new_snake
 end
 
 def grow(snake, direction)
-  last_position = snake.last
-  new_position = [last_position[0]+direction[0], last_position[1]+direction[1]]
-  snake.push(new_position)
+  new_snake = []
+  snake.map { |item| new_snake << item }
+  new_snake << [snake.last[0] + direction[0], snake.last[1] + direction[1]]
 end
 
 def new_food(food, snake, dimensions)
-  new_food = [rand(dimensions[:width]), rand(dimensions[:height])]
-  while food.include?(new_food) or snake.include?(new_food)
-    new_food = [rand(dimensions[:width]), rand(dimensions[:height])]
-  end
-  new_food
+  x = 0.upto(dimensions[:width] - 1).to_a
+  y = 0.upto(dimensions[:height] - 1).to_a
+  (possible_food = x.product(y) - food - snake).sample
 end
 
 def obstacle_ahead?(snake, direction, dimensions)
-  head = snake.last
-  move = [head[0]+direction[0], head[1]+direction[1]]
-  obstacle = false
+  head, obstacle, after = snake.last, false, move(snake, direction).last
 
-  if snake.include?(move) then obstacle = true end
-  if move[0] < 0 or move[0] >= dimensions[:width] then obstacle = true end
-  if move[1] < 0 or move[1] >= dimensions[:height] then obstacle = true end
+  if snake.include?(after) then obstacle = true end
+  if after[0] < 0 or after[0] >= dimensions[:width] then obstacle = true end
+  if after[1] < 0 or after[1] >= dimensions[:height] then obstacle = true end
 
   obstacle
 end
 
 def danger?(snake, direction, dimensions)
-  two_moves = [direction[0]*2, direction[1]*2]
+  two_moves = [direction[0] * 2, direction[1] * 2]
   return true if obstacle_ahead?(snake, direction, dimensions)
   return true if obstacle_ahead?(snake, two_moves, dimensions)
   false
